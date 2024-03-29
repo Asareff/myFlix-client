f0f7518a5e51988238d8a5aa7a0fc0d331aaee05
Can Noah <87754848+cannoahgkt@users.noreply.github.com>
2024-03-29 10:58:09 +0100

    MovieCard changes

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,65 +1,66 @@
 import "./MovieCard.scss";
 import PropTypes from "prop-types";
-import { Card } from "react-bootstrap";
+import { Card, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export const MovieCard = ({ movie, isFav, handleFavClick }) => {
+export const MovieCard = ({ movie, user, setUser, token }) => {
+  const isFavorite = user?.FavoriteMovies?.find((m) => m === movie._id);
+  const [image, setImage] = useState("");
+
+  const handleFavoriteButton = () => {
+    const method = isFavorite ? "delete" : "post";
+    fetch(
+      `https://movies-service-330159435834.herokuapp.com/users/${user.Username}/movies/${movie._id}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+        method,
+      }
+    )
+      .then((result) => result.json())
+      .then((data) => setUser(data));
+  };
+
+  useEffect(() => {
+    if (movie.imageUrl) {
+      setImage(movie.imageUrl);
+    }
+  }, [movie]);
 
   return (
     <Card className="h-100">
-      <Link to={`/moviedata/${movie.Title}`}>
-        <Card.Img 
-        variant="top" 
-        src={movie.ImagePath}
-        id="movie-cover" 
-        />
-      </Link>
+      <Card.Img variant="top" src={image} />
       <Card.Body>
-        <div className="fav-icon"
-          style={{
-            display: 'inline-flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: '50%',
-            overflow: 'hidden',
-            padding: '5px',
-            cursor: 'pointer'
-          }}
-          onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = 'white')}
-          onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}
-        >
-          {isFav ? (
-            <HeartFill
-              size={20}
-              color="red"
-              onClick={() => handleFavClick(movie, true)}
-            />
-          ) : (
-            <Heart
-              size={20}
-              color="red"
-              onClick={() => handleFavClick(movie, false)}
-            />
-          )}
-        </div>
-        <Card.Title>{movie.Title}</Card.Title>
-        <Card.Title>{movie.Year}</Card.Title>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.director.name}</Card.Text>
+        <Row>
+          <Col>
+            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+              <Button variant="link">Open</Button>
+            </Link>
+          </Col>
+        </Row>
+        <Col>
+          <Button variant="link" onClick={handleFavoriteButton}>
+            {isFavorite ? "Remove" : "Add"} favorite
+          </Button>
+        </Col>
       </Card.Body>
-      <Card.Footer>
-        <Link to={`/moviedata/${movie.Title}`} className="open-button">
-          Open
-        </Link>
-      </Card.Footer>
     </Card>
   );
 };
 
+// Here is where we define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    _id: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Year: PropTypes.string.isRequired
+    title: PropTypes.string,
+    director: PropTypes.shape({
+      name: PropTypes.string
+    }),
+    imageUrl: PropTypes.string,
+    _id: PropTypes.string
   }).isRequired,
-  handleFavClick: PropTypes.func.isRequired,
-  isFav: PropTypes.bool.isRequired
+  user: PropTypes.object.isRequired,
+  setUser: PropTypes.func.isRequired,
+  token: PropTypes.string.isRequired
 };
