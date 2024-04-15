3c769194a7d9ca3628c26f8c2cf335a146615402
Can Noah <87754848+cannoahgkt@users.noreply.github.com>
2024-04-15 02:15:50 +0200

    filter and more movies

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,24 +1,44 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from 'react-bootstrap';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useParams } from "react-router-dom";
 import { MovieCard } from "../MovieCard/MovieCard";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../SignupView/signup-view";
 import { NavigationBar } from "../nav-bar/navigation-bar";
 import ProfileView from '../ProfileView/ProfileView';
 import { MovieView } from '../MovieView/MovieView';
+import "./MainView.scss";
 
 const MainView = () => {
   const storedUser = localStorage.getItem("user");
   const [user, setUser] = useState(storedUser ? JSON.parse(storedUser) : null);
   const [token, setToken] = useState(localStorage.getItem("token") || null);
   const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState([]);
+  const { movieId } = useParams();
+  const movie = movies.find((movie) => movie._id === movieId);
+
 
   useEffect(() => {
     fetch('https://movies-service-330159435834.herokuapp.com/movies')
       .then(response => response.json())
-      .then(data => setMovies(data.movies));
+      .then(data => {
+        setMovies(data.movies);
+        setFilteredMovies(data.movies); // Initialize filteredMovies with all movies
+      });
   }, []);
+
+  useEffect(() => {
+    console.log("Selected Genre:", selectedGenre);
+    setFilteredMovies(movies.filter(movie => {
+      const titleMatches = !search || movie.title.toLowerCase().includes(search.toLowerCase());
+      const genreMatches = !selectedGenre || (movie.genre && movie.genre.name.toLowerCase() === selectedGenre.toLowerCase());
+      return titleMatches && genreMatches;
+    }));
+  }, [movies, search, selectedGenre]);
+  
 
   return (
     <BrowserRouter>
@@ -51,7 +71,6 @@ const MainView = () => {
                 user ? <ProfileView user={user} token={token} /> : <Navigate to="/login" />
               }
             />
-
             <Route
               path="/movies/:movieId"
               element={
@@ -61,8 +80,6 @@ const MainView = () => {
                   <MovieView
                     movies={movies}
                     user={user}
-                    addFav={(id) => addFav(id)}
-                    removeFav={(id) => removeFav(id)}
                   />
                 )
               }
@@ -73,16 +90,33 @@ const MainView = () => {
                 !user ? (
                   <Navigate to="/login" replace />
                 ) : (
-                  <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
-                    {movies.map(movie => (
-                      <div key={movie._id} style={{ width: '300px', margin: '10px' }}>
-                        <MovieCard
-                          movie={movie}
-                          onAddToFavorites={() => addFav(movie._id)}
-                          onRemoveFromFavorites={() => removeFav(movie._id)}
-                        />
-                      </div>
-                    ))}
+                  <div>
+                    <div className="filter-container">
+                      <input
+                        type="text"
+                        placeholder="Search by title"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                      />
+                      <select
+                        value={selectedGenre}
+                        onChange={(e) => setSelectedGenre(e.target.value)}
+                      >
+                        <option value="">Filter by genre</option>
+                        {Array.from(new Set(movies.map(movie => movie['genre.name']))).map(genre => (
+                          <option key={genre} value={genre}>{genre}</option>
+                        ))}
+                      </select>
+
+                    </div>
+                    <div className="movie-cards-container">
+                      {filteredMovies.map(movie => (
+                        <div key={movie._id} className="movie-card">
+                          <MovieCard movie={movie} genreName={movie['genre.name']} />
+
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 )
               }
--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,24 +1,13 @@
 import React from "react"
 import "./MovieCard.scss";
-import PropTypes from "prop-types";
 import { Card, Button, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 
-export const MovieCard = ({ movie, user, setUser, token, onAddToFavorites, onRemoveFromFavorites }) => {
-  const isFavorite = user?.FavoriteMovies?.find((m) => m === movie?._id);
+export const MovieCard = ({ movie }) => {
   const [image, setImage] = useState("");
-
-  const handleFavoriteButton = () => {
-    if (isFavorite) {
-      // If already in favorites, remove it
-      onRemoveFromFavorites(movie._id);
-    } else {
-      // If not in favorites, add it
-      onAddToFavorites(movie._id);
-    }
-  };
 
   useEffect(() => {
     if (movie && movie.imageUrl) {
@@ -31,7 +20,6 @@ export const MovieCard = ({ movie, user, setUser, token, onAddToFavorites, onRem
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
-        <Card.Text>{movie.director?.name}</Card.Text> {/* Add null check here */}
         <Row>
           <Col>
             <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
@@ -39,31 +27,17 @@ export const MovieCard = ({ movie, user, setUser, token, onAddToFavorites, onRem
             </Link>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Button variant="link" onClick={handleFavoriteButton}>
-              {isFavorite ? "Remove" : "Add"} to Favorites
-            </Button>
-          </Col>
-        </Row>
       </Card.Body>
     </Card>
   );
 };
 
-// Here is where we define all the props constraints for the MovieCard
+// define all the props constraints for the MovieCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
-    director: PropTypes.shape({
-      name: PropTypes.string
-    }),
     imageUrl: PropTypes.string,
     _id: PropTypes.string
   }).isRequired,
-  user: PropTypes.object.isRequired,
-  setUser: PropTypes.func.isRequired,
-  token: PropTypes.string.isRequired,
-  onAddToFavorites: PropTypes.func.isRequired,
-  onRemoveFromFavorites: PropTypes.func.isRequired,
+  genreName: PropTypes.string.isRequired, // Add genreName prop
 };
